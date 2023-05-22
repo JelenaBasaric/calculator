da3b8fcbdd7fbdf76381eeb1c2810deec6f4e6bb
Jelena Basaric <jbasaric83@gmail.com>
2023-05-22 12:39:45 +0200

    result

--- src/app/calculatorService.service.ts
+++ src/app/calculatorService.service.ts
@@ -4,6 +4,7 @@ import { Observable, first } from "rxjs";
 export class calculatorService {
     valueFormula = new EventEmitter<string>();
     valueResult = new EventEmitter<string>();
+    numberAndOperationrray:string[]=[]
     formula: string = "0";
     result = "0";
     value: string = '0';
@@ -22,6 +23,7 @@ export class calculatorService {
         if (this.formula.endsWith("0") && value != "." && value != "+"
             && value != "-" && value != "*" && value != "/") {
             this.formula = this.formula.replace(/.$/, value);
+
             this.valueFormula.emit(this.formula);
         }
         else {
@@ -32,25 +34,27 @@ export class calculatorService {
     }
     addOperationSign(value: string) {
         if (value != "-") {
-            if (this.formula.endsWith("+-") || this.formula.endsWith("/-") || this.formula.endsWith("*-") || this.formula.endsWith("--")) { return; }
-            else if (this.formula.endsWith("-") || this.formula.endsWith("/") || this.formula.endsWith("*") || this.formula.endsWith("+")) {
-                this.formula = this.formula.replace(/.$/, value);
+            if (this.formula.endsWith("+- ") || this.formula.endsWith("/- ") || this.formula.endsWith("*- ") || this.formula.endsWith("-- ")) { return; }
+            else if (this.formula.endsWith(" - ") || this.formula.endsWith(" / ") || this.formula.endsWith(" * ") || this.formula.endsWith(" + ")) {
+                this.formula =this.formula.trimEnd().replace(/.$/, value+" ");
+               // this.formula = this.formula.replace(/.$/, value+" ");
                 this.valueFormula.emit(this.formula); return;
             }
 
             else {
-                this.formula += value;
+                this.formula +=" "+ value+" ";
                 this.valueFormula.emit(this.formula);
             }
         }
         else {
-            if (this.formula.endsWith("+-") || this.formula.endsWith("/-") || this.formula.endsWith("*-") || this.formula.endsWith("--")) { return; }
-            else if (this.formula.endsWith("-") || this.formula.endsWith("/") || this.formula.endsWith("*") || this.formula.endsWith("+")) {
-                this.formula += value;
+            if (this.formula.endsWith("+- ") || this.formula.endsWith("/- ") || this.formula.endsWith("*- ") || this.formula.endsWith("-- ")) { return; }
+            else if (this.formula.endsWith(" - ") || this.formula.endsWith(" / ") || this.formula.endsWith(" * ") || this.formula.endsWith(" + ")) {
+                this.formula=this.formula.trimEnd()+value+" ";
+              //  this.formula += value+" ";
                 this.valueFormula.emit(this.formula); return;
             }
             else {
-                this.formula += value;
+                this.formula +=" "+ value+" ";
                 this.valueFormula.emit(this.formula);
             }
         }
@@ -61,16 +65,37 @@ export class calculatorService {
     }
     equalsAll() {
         if(this.formula.includes('.')){
+            this.numberAndOperationrray=this.formula.split(" ");
             this.resultAll=parseFloat(this.formula);
-            console.log(this.resultAll);
-            this.result=(this.resultAll.toString());
-            this.formula="";
-            this.valueResult.emit(this.result);
+            for(let i=0;i<this.numberAndOperationrray.length;i++){
+            console.log(this.numberAndOperationrray[i]);
+           // this.resultAll+=parseFloat(this.numberAndOperationrray[i]);
+            if(this.numberAndOperationrray[i].startsWith('*'))
+
+            {let m=parseFloat(this.numberAndOperationrray[i-1]);
+                let n=parseFloat(this.numberAndOperationrray[i-1]);
+                let r=m*n;
+            }
+            this.resultAll+=r;
+        }
+         console.log(this.resultAll);
+        
+           // this.result=(this.resultAll.toString());
+            //this.formula="0";
+            //this.valueResult.emit(this.result);
+            //this.valueFormula.emit(this.formula);
         }
         else{
-            this.resultAll=(parseInt(this.formula));
-            console.log(this.resultAll);
-            this.result=(this.resultAll.toString());
+            this.numberAndOperationrray=this.formula.split(" ");
+            //this.resultAll=parseFloat(this.formula);
+            for(let i=0;i<this.numberAndOperationrray.length;i++){
+            console.log(this.numberAndOperationrray[i]);}
+            // this.resultAll=(parseInt(this.formula));
+            // console.log(this.resultAll);
+            // this.result=(this.resultAll.toString());
+            // this.formula="0";
+            // this.valueResult.emit(this.result);
+            // this.valueFormula.emit(this.formula);
         }
       
 
@@ -79,7 +104,5 @@ export class calculatorService {
     deleteAll() {
         this.formula = "0";
         this.valueFormula.emit(this.formula);
-      
-
     }
 }
