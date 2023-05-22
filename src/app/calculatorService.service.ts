f6de9934756e89100abfc2ae726e9d960bd1257e
Jelena Basaric <jbasaric83@gmail.com>
2023-05-22 10:45:46 +0200

    delete errors

--- src/app/calculatorService.service.ts
+++ src/app/calculatorService.service.ts
@@ -5,31 +5,13 @@ export class calculatorService {
     valueFormula = new EventEmitter<string>();
     valueResult = new EventEmitter<string>();
     formula: string = "0";
-    numNumberArray:number[]=[];
     result = "0";
-    resultAll:number=0;
     value: string = '0';
-    numString:string="";
-    numNumber:number=0;
     getValues(): any {
         return this.formula;
 
     }
-    setNumber(numString:string):number{
-        if (this.numString.includes('.')) {
-         this.numNumber=parseFloat(this.numString);
-        this.numNumberArray.push(this.numNumber);
-        this.resultAll=parseFloat(this.result);
-        return this.numNumber;
-        }
-        else {
-        this.numNumber=parseInt(this.numString);
-        this.numNumberArray.push(this.numNumber);
-        this.resultAll=parseInt(this.result);
-        return this.numNumber;
-        }
-        
-    }
+    
     getResult() {
         return this.result;
     }
@@ -39,7 +21,6 @@ export class calculatorService {
         if (this.formula.endsWith("0") && value != "." && value != "+"
             && value != "-" && value != "*" && value != "/") {
             this.formula = this.formula.replace(/.$/, value);
-            this.setNumber(value);
             this.valueFormula.emit(this.formula);
         }
         else {
@@ -72,25 +53,20 @@ export class calculatorService {
                 this.valueFormula.emit(this.formula);
             }
         }
-        
 
 
 
 
     }
     equalsAll() {
-      console.log(this.result);
-      console.log(this.resultAll.toString())
-      this.valueResult.emit(this.result);
-      this.formula="0";
+      
 
 
     }
     deleteAll() {
         this.formula = "0";
         this.valueFormula.emit(this.formula);
-        this.numNumberArray=[];
-        this.resultAll=0;
+      
 
     }
 }
